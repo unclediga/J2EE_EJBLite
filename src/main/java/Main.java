77705dde03ea7fa1db8e01c7533e9f55a17b6ceb
unclediga <zowie_i@mail.ru>
2021-09-23 21:58:33 +0300

    EJB Exception: javax.persistence.TransactionRequiredException: No transaction is currently active

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import data.Book;
 import ejb.EJB1;
 import ejb.EJB2;
 
@@ -5,30 +6,76 @@ import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
 import javax.naming.NamingException;
 import java.io.File;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static final String URL_H2 = "jdbc:h2:~/test";
+
     public static void main(String[] args) throws NamingException {
         System.out.println("Hello!");
+        System.out.println("Connection test...");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL_H2, "sa", "");
+            DriverManager.registerDriver(new org.h2.Driver());
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOK");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                System.out.println("row " + resultSet.getRow() + " id:" + id + " name:" + name);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
 
         Map<String, Object> properties = new HashMap<>();
         properties.put(EJBContainer.MODULES, new File("target/classes"));
 
+        EJBContainer container = null;
+        try {
+            container = EJBContainer.createEJBContainer(properties);
+            Context context = container.getContext();
 
-        EJBContainer container = EJBContainer.createEJBContainer(properties);
-        Context context = container.getContext();
-        EJB1 ejb1 = (EJB1) context.lookup("java:global/classes/EJB1");
-        System.out.println(ejb1.getHello());
+            EJB1 ejb1 = (EJB1) context.lookup("java:global/classes/EJB1");
+            System.out.println(ejb1.getHello());
 
-        EJB2 ejb2 = (EJB2) context.lookup("java:global/classes/EJB2");
-        System.out.println(ejb2.findAll());
+            EJB2 ejb2 = (EJB2) context.lookup("java:global/classes/EJB2");
+            for(Book b: ejb2.findAll()){
+                System.out.println(b);
+            }
 
-        container.close();
+            System.out.println("Creating new book...");
+            Book myNewBook = ejb2.createBook("My new book");
+            System.out.println("Was created: " + myNewBook);
 
+            for(Book b: ejb2.findAll()){
+                System.out.println(b);
+            }
+
+        } catch (Exception e) {
+            System.err.println("My:Error create WL!");
+            e.printStackTrace();
+        } finally {
+            if (container != null) {
+                container.close();
+            }
+        }
         System.out.println("Bye!");
-
-
+        System.exit(0);
     }
 
 }
--- src/main/java/ejb/EJB2.java
+++ src/main/java/ejb/EJB2.java
@@ -1,27 +1,57 @@
 package ejb;
 
+import data.Book;
+
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.Query;
-
-import data.Book;
-
+import javax.transaction.*;
 import java.util.List;
 
 @Stateless
 public class EJB2 {
-    @PersistenceContext(unitName = "PU")
+    @PersistenceContext(unitName = "PU",type = PersistenceContextType.TRANSACTION)
     private EntityManager em;
 
-    public Book createBook(Book book) {
+    @Resource
+    UserTransaction utx;
+
+    public Book createBook(String name) {
+        int id = getMaxId() + 1;
+        Book book = new Book();
+        book.setId(id);
+        book.setName(name + "[" + id + "]");
+        em.persist(book);
+        em.flush();
+//        em.getTransaction().begin();
+//        try {
+//            utx.begin();
+//            em.persist(book);
+//            em.flush();
+//            utx.commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            try {
+//                utx.rollback();
+//            } catch (SystemException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//        em.getTransaction().commit();
         return book;
     }
 
+    public int getMaxId() {
+        List list = em.createNativeQuery("SELECT MAX(id) FROM Book").getResultList();
+        return (int) list.get(0);
+    }
+
     public List<Book> findAll() {
-        Query nativeQuery = em.createNativeQuery("SELECT id,name FROM Book");
-        List resultList = nativeQuery.getResultList();
-        return resultList;
+        Query query = em.createQuery("SELECT o FROM Book o");
+        return query.getResultList();
     }
 
 }
